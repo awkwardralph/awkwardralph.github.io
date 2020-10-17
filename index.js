26d777a54434ab3506744fc3cf28d9ffdd26c2ab
Ralph I'mRandom Lee <ralphlee22@gmail.com>
2020-10-17 02:12:46 -0500

    jeopardy

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-const button = document.querySelector('button');
+// url to the api
+const url = 'http://jservice.io/api/random';
 
-button.addEventListener('click', () =>{
-    console.log( 'hi' );
-})
+// some global variables
+let questionObject = {};
+
+// query selector for
+const questionHTML = document.getElementById('question');
+const categoryHTML = document.getElementById('category');
+const answerHTML = document.getElementById('answer');
+
+const showButton = document.getElementById('showButton');
+showButton.addEventListener('click', showAnswer);
+
+const restartButton = document.getElementById('restartButton');
+restartButton.addEventListener('click', restartQuestion)
+
+function getQuestion(){
+    let question;
+    let answer;
+    let category;
+    let points;
+    answerHTML.innerText = '';
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            questionObject.question = data[0].question;
+            questionObject.answer = data[0].answer;
+            questionObject.category = data[0]['category'].title;
+            questionObject.points = data[0].value;
+
+            categoryHTML.innerText = `${questionObject.category} - $${questionObject.points}`;
+            questionHTML.innerText = `${questionObject.question}`;
+
+
+        })
+}
+
+function showAnswer(){
+    answerHTML.innerText = `${questionObject.answer}`
+}
+
+function restartQuestion(){
+    getQuestion()
+}
+
+getQuestion();
